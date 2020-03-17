2ae0e525a7155e59c9af29f406430d8d7c5afbf7
松本一将 <kazumasa.matsumoto62@gmail.com>
2020-03-17 19:26:57 +0900

    コメント追加

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+// アカウント作成に必要な機能がsymbol-sdkにあります
 import {Account, NetworkType} from 'symbol-sdk';
 
 @Component({
@@ -11,6 +12,7 @@ export class Tab1Page {
   constructor() {}
 
   createWallet() {
+    // アカウント作成
     const account = Account.generateNewAccount(NetworkType.TEST_NET);
     console.log('Your new account address is:', account.address.pretty(), 'and its private key', account.privateKey);
   }
