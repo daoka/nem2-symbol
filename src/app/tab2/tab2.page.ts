099d07099472a7a37b548ce9ce6d03184a786e7e
松本一将 <kazumasa.matsumoto62@gmail.com>
2020-03-17 13:55:06 +0900

    sdkのバージョンをsymbolに変更

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,16 @@
 import { Component } from '@angular/core';
+import {
+  Account,
+  Address,
+  Deadline,
+  Mosaic,
+  MosaicId,
+  NetworkType,
+  PlainMessage,
+  RepositoryFactoryHttp,
+  TransferTransaction,
+  UInt64,
+} from 'symbol-sdk';
 
 @Component({
   selector: 'app-tab2',
@@ -8,5 +20,25 @@ import { Component } from '@angular/core';
 export class Tab2Page {
 
   constructor() {}
+
+  semdSymbol() {
+    const rawAddress = 'TBONKW-COWBZY-ZB2I5J-D3LSDB-QVBYHB-757VN3-SKPP';
+    const recipientAddress = Address.createFromRawAddress(rawAddress);
+    // replace with network type
+    const networkType = NetworkType.TEST_NET;
+    // replace with symbol.xym id
+    const networkCurrencyMosaicId = new MosaicId('51A99028058245A8');
+    // replace with network currency divisibility
+    const networkCurrencyDivisibility = 6;
+
+    const transferTransaction = TransferTransaction.create(
+        Deadline.create(),
+        recipientAddress,
+        [new Mosaic (networkCurrencyMosaicId,
+            UInt64.fromUint(10 * Math.pow(10, networkCurrencyDivisibility)))],
+        PlainMessage.create('This is a test message'),
+        networkType,
+        UInt64.fromUint(2000000));
+  }
 
 }
